fbe252edc47df69a017406e5147a11b5a178ba73
Guillaume-Cudel <58982293+Guillaume-Cudel@users.noreply.github.com>
2022-08-18 15:58:46 +0200

    Fix problem with navigation drawer. Looking for fix problem with database

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -2,38 +2,37 @@ package ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Gravity
 import android.view.MenuItem
+import android.widget.*
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 import com.guillaume.mathworld.R
 import com.guillaume.mathworld.databinding.ActivityMainBinding
-import dao.ClassDAO
-import database.AppDatabase
 import di.MathWorldApplication
 import di.MathWorldViewModelFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
+import model.RpgClass
+import ui.fragments.ClassFragment
 import ui.fragments.ClassManagementFragment
 import ui.fragments.NotebookFragment
 import ui.fragments.NumNinjaFragment
 import viewModel.MainViewModel
-import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     private val managementFragment = ClassManagementFragment()
     private val notebookFragment = NotebookFragment()
     private val numNinjaFragment = NumNinjaFragment()
+
     private val mainVM: MainViewModel by viewModels {
         MathWorldViewModelFactory((application as MathWorldApplication).repository)
     }
@@ -46,18 +45,10 @@ class MainActivity : AppCompatActivity() {
 
         configureNavigation()
         replaceFragment(managementFragment)
+        //addClass()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.nav_header_recycler_view)
-        val adapter = ClassesListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.navView.setNavigationItemSelectedListener(this)
 
-
-        mainVM.allClasses.observe(this, Observer { classes ->
-            classes?.let {
-                adapter.submitList(it)
-            }
-        })
 
     }
 
@@ -68,14 +59,21 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+
+
     private fun configureNavigation(){
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val bottomNavigation: NavigationBarView = findViewById(R.id.bottom_navigation)
+
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
 
         bottomNavigation.setOnItemSelectedListener {
             when(it.itemId){
@@ -87,11 +85,76 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     private fun replaceFragment(fragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
         transaction.commit()
     }
+
+    /*private fun addClass(){
+        val addButton = findViewById<FloatingActionButton>(R.id.nav_header_fab_add)
+
+        addButton.setOnClickListener {
+            //todo change les boutons du navheader sur le fragment et passe les action des boutons sur le fragments
+            createDialog()
+        }
+    }*/
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        // Handle navigation view item clicks here.
+        //var fragment:Fragment = ImportFragment.newInstance()
+
+        when (item.itemId) {
+            R.id.header_item_add -> {
+                createDialog()
+
+            }
+            R.id.header_item_remove -> {
+            }
+        }
+
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun createDialog(){
+        val builder = AlertDialog.Builder(this).create()
+        val view = layoutInflater.inflate(R.layout.dialog_add_class, null)
+        val name = view.findViewById<EditText>(R.id.add_class_name_edit)
+        val levelChoice = resources.getStringArray(R.array.Level)
+        val arrayAdapter = ArrayAdapter(this, R.layout.level_list_item, levelChoice)
+        val level: AutoCompleteTextView = view.findViewById(R.id.add_class_level_choice_text)
+        level.setAdapter(arrayAdapter)
+        val saveButton = view.findViewById<Button>(R.id.add_class_save)
+        builder.setView(view)
+
+        saveButton.setOnClickListener {
+            if(name.editableText?.toString() != ""){
+                val newClass = RpgClass(name.editableText.toString(), null, level.editableText.toString())
+                mainVM.insertClass(newClass)
+            } else {
+                Toast.makeText(this, getString(R.string.enter_name), Toast.LENGTH_LONG).show()
+            }
+        }
+        builder.setCanceledOnTouchOutside(true)
+        builder.show()
+
+    }
+
+
+
+
+
 
 
 }
